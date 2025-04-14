27d7fc06d635371cf81b6913fb4d498d400ecb46
R R <ratri.retnowati@gmail.com>
2025-04-14 17:49:52 +0700

    edit respon signup

--- signUp.php
+++ signUp.php
@@ -7,5 +7,7 @@ if (isset($_POST['fullname']) && isset($_POST['email']) && isset($_POST['usernam
             echo "Sign Up Success";
         } else echo "Sign up Failed";
     } else echo "Error: Database connection";
+} else if !isset($_POST['fullname']) || !isset($_POST['email']) || isset($_POST['username'])|| !isset($_POST['password']) {
+    echo "empty $_POST['fullname'] $_POST['email'] $_POST['username'] $_POST['password'] "
 } else echo "All fields are required";
 ?>
