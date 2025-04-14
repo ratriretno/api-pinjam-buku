c1e024677e9e1cd9187aa8f0ea21063548159263
R R <ratri.retnowati@gmail.com>
2025-04-14 17:37:20 +0700

    update code

---
+++ DataBaseConfig.php
@@ -0,0 +1,22 @@
+<?php
+
+class DataBaseConfig
+{
+    public $servername;
+    public $username;
+    public $password;
+    public $databasename;
+
+    public function __construct()
+    {
+
+        $this->servername = 'localhost';
+        $this->username = 'u233761808_admin';
+        $this->password = 'D!c0d1ng2025';
+        $this->databasename = 'u233761808_pinjambuku';
+
+    }
+}
+
+?>
+
