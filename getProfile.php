0a117fe25162b04a7bbdc5cc9b0c53f8575d31a0
R R <ratri.retnowati@gmail.com>
2025-04-20 00:58:16 +0700

    fix query

--- getProfile.php
+++ getProfile.php
@@ -6,8 +6,7 @@ $db = new DataBase();
 if (isset($_POST['username'])) {
     if ($db->dbConnect()) {
         $profile = $db->getProfile("users", $_POST['username']);
-        $obj_profile = json_decode (json_encode ($profile), FALSE);
-        echo $obj_profile;
+        echo json_encode ($profile);
     } else echo "Error: Database connection";
 } else echo "username fields are required";
 ?>
