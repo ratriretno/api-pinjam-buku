95c5ef755dd383b0400586d74c9990a112169754
R R <ratri.retnowati@gmail.com>
2025-04-20 14:43:27 +0700

    check result

--- DataBase.php
+++ DataBase.php
@@ -72,11 +72,13 @@ class DataBase
         $this->sql = "select * from " . $table . " where username = '" . $username . "'";
         $result = mysqli_query($this->connect, $this->sql);
         $row = mysqli_fetch_assoc($result);
-        if (mysqli_num_rows($result) != 0) {
-            $result = $result->fetch_all(MYSQLI_ASSOC);
-        } else if (mysqli_num_rows($result) == 0) {
-            $result = "{empty}";
-        }
+        // if (mysqli_num_rows($result) != 0) {
+        //     $result = $result->fetch_all(MYSQLI_ASSOC);
+        // } else if (mysqli_num_rows($result) == 0) {
+        //     $result = "{empty}";
+        // } else {
+        //     $result = "{other}"
+        // }
 
         return $result;
         
