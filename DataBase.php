60cf33d237915625600f6a879573ec11932a1e1d
R R <ratri.retnowati@gmail.com>
2025-04-20 13:55:10 +0700

    fix profile

--- DataBase.php
+++ DataBase.php
@@ -72,11 +72,11 @@ class DataBase
         $this->sql = "select * from " . $table . " where username = '" . $username . "'";
         $result = mysqli_query($this->connect, $this->sql);
         $row = mysqli_fetch_assoc($result);
-        // if (mysqli_num_rows($result) != 0) {
-            $profile = $result->fetch_all(MYSQLI_ASSOC);
-        // }
+        if (mysqli_num_rows($result) != 0) {
+            $result = $result->fetch_all(MYSQLI_ASSOC);
+        }
 
-        return $profile;
+        return $result;
         
     }
 
