53ab6471800d121adf6cd7b5841fe72786a003fd
R R <ratri.retnowati@gmail.com>
2025-04-23 23:35:52 +0700

    update response login

--- DataBase.php
+++ DataBase.php
@@ -34,22 +34,38 @@ class DataBase
         return mysqli_real_escape_string($this->connect, stripslashes(htmlspecialchars($data)));
     }
 
-    function logIn($table, $username, $password)
+    function logIn($table, $email, $password)
     {
-        $username = $this->prepareData($username);
-        $password = $this->prepareData($password);
-        $this->sql = "select * from " . $table . " where username = '" . $username . "'";
+        // $username = $this->prepareData($username);
+        // $password = $this->prepareData($password);
+        $this->sql = "select * from " . $table . " where email = '" . $email . "'";
         $result = mysqli_query($this->connect, $this->sql);
         $row = mysqli_fetch_assoc($result);
         if (mysqli_num_rows($result) != 0) {
-            $dbusername = $row['username'];
+            $dbemail = $row['email'];
             $dbpassword = $row['password'];
-            if ($dbusername == $username && password_verify($password, $dbpassword)) {
+            if ($dbemail == $email && password_verify($password, $dbpassword)) {
                 $login = true;
-            } else $login = false;
-        } else $login = false;
+                $booksArray= "{
+                    \"error\": false,
+                    \"message\": \"Login Success\",
+                    \"login\": true,
+                    \"profile\": ".json_encode($row)."}";
+            } else {
+                $booksArray= "{
+                    \"error\": false,
+                    \"login\": false,
+                    \"message\": \"Password atau Email Salah\",
+                    \"profile\": "[]"}";
+            }
+        } else  $booksArray= "{
+            \"error\": false,
+            \"login\": false,
+            \"message\": \"Password atau Email Salah\",
+            \"profile\": "[]"}";;
 
-        return $login;
+
+        return $booksArray;
     }
 
     function signUp($table, $fullname, $email, $username, $password)
