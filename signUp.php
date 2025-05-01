cfc80d5540d57da00f43969785ab7b4deea4878e
R R <ratri.retnowati@gmail.com>
2025-05-01 16:08:16 +0700

    fix response  signup php

--- signUp.php
+++ signUp.php
@@ -3,9 +3,8 @@ require "DataBase.php";
 $db = new DataBase();
 if (isset($_POST['fullname']) && isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password'])) {
     if ($db->dbConnect()) {
-        if ($db->signUp("users", $_POST['fullname'], $_POST['email'], $_POST['username'], $_POST['password'])) {
-            echo "Sign Up Success";
-        } else echo "Sign up Failed";
+        $result = $db->signUp("users", $_POST['fullname'], $_POST['email'], $_POST['username'], $_POST['password']);
+       echo $result;
     } else echo "Error: Database connection";
 } else if (empty($_POST['fullname']) || empty($_POST['email']) || empty($_POST['username'])|| empty($_POST['password'])) {
     echo "empty";
